d8436a32d8b9ee061d0e49fa4a08c721ebd57d94
saadkh12 <saadullahkh55@gmail.com>
2025-05-12 22:14:10 +0500

    12/2025

--- To-Do web app by Sir Bilal/toDo.js
+++ To-Do web app by Sir Bilal/toDo.js
@@ -1,17 +1,68 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// All Selectors Are Here
+
+// AddTask All Selectors Here 
 const addTask = document.querySelectorAll('#title')
-const btnSub = document.querySelector('.btnSub')
+const first = document.querySelector('.title1')
+const description = document.querySelector('.title2')
+const priority = document.querySelector('.title3')
+const btnSub = document.getElementById('btnSub')
+const resbtn = document.getElementById('resBtn')
+const taskSave = []
+// AddTask All Selectors ended
 
-let taskSave = []
+// Filter Tasl All Selectors here
+const filteredCategory = document.getElementById('All')
+const projectTaskCon = document.getElementById('projectTaskCon')
+// const filterSave = [];
+const filteredSaved = {};
+filteredSaved.filter(filteredCategory)
 
-addTask.forEach(function(item){
-    btnSub.addEventListener('click',function(){
-        if (item.id ){
-            item.id.push(taskSave)
-            taskSave.addTask = "";
-            console.log("success")
-        } else{
-            console.log("Failed");
-            
-        }
+// All Selectors Are ended
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// Add Task Submit button Functionality here
+function submitt() {
+    btnSub.addEventListener('click', () => {
+        Object.keys(taskSave.push(first.value))
+        first.value = "";
+        // console.log(taskSave);
+        Object.keys(taskSave.push(description.value))
+        description.value = "";
+        // console.log(taskSave);
+        Object.keys(taskSave.push(priority.value))
+        priority.value = "";
+        // console.log(taskSave);
+        priority.value = "Low";
     })
-})
+}
+// Add Task Submit button Functionality ended
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// Add Task Reset button Functionality here
+function resett() {
+    resbtn.addEventListener('click', function () {
+        first.value = "";
+        description.value = "";
+        priority.value = "";
+        // console.log(priority);
+        priority.value = "Low";
+        // console.log(first, description, priority);
+    })
+}
+// Add Task Reset button Functionality ended
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// FilterTask status and priority here
+function filteredTask() {
+    console.log("filteredCategory", filteredCategory);
+    if (filteredSaved === projectTaskCon) {
+        projectTaskCon = filteredCategory.value
+        console.log("filterSave", filterSave);
+    }
+}
+// FilterTask status and priority ended
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
